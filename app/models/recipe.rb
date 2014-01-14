f9fd4fad859dc262b04f3bd119d0a7a090fc3a18
BRIAN WONG <brianwong@BRIANs-MacBook-Pro.local>
2014-01-13 19:44:50 -0600

    implement basic search function

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,12 @@
 class Recipe < ActiveRecord::Base
 	has_many :recipe_ingredients
 	has_many :ingredients, through: :recipe_ingredients
-end
+
+	def self.search(search)
+		if search
+			find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+		else
+			find(:all)
+		end
+	end
+end 
