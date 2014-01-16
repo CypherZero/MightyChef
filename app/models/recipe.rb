0f629a20b7697c4494b89d4b2463b543be7b8257
BRIAN WONG <brianwong@BRIANs-MacBook-Pro.local>
2014-01-15 20:07:12 -0600

    Modify search to include search in description.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,9 +2,10 @@ class Recipe < ActiveRecord::Base
 	has_many :recipe_ingredients
 	has_many :ingredients, through: :recipe_ingredients
 
+	accepts_nested_attributes_for :recipe_ingredients
 	def self.search(search)
 		if search
-			find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+			find(:all, :conditions => ['name LIKE ? or description LIKE ?', "%#{search}%", "%#{search}%"])
 		else
 			find(:all)
 		end
